bbb8d263ac8d6454a3418e8747a5b786bc5072fc
HyperAlch <Daemonleak@protonmail.com>
2024-03-07 17:13:33 -0500

    Added error handling to ping slash command

--- slashcommands/ping.go
+++ slashcommands/ping.go
@@ -3,13 +3,18 @@ package slashcommands
 import (
 	"MythiccBotHyper/globals"
 	"github.com/bwmarrin/discordgo"
+	"log"
 )
 
 func ping(_ *discordgo.Session, i *discordgo.InteractionCreate) {
-	globals.Bot.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+	err := globals.Bot.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Content: "Bot is online...",
 		},
 	})
+
+	if err != nil {
+		log.Println("ERROR:", err)
+	}
 }
