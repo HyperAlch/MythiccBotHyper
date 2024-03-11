a190b280ae72f84aa71723887f8c1c94458a8489
HyperAlch <Daemonleak@protonmail.com>
2024-03-11 16:21:15 -0400

    admins slash command safety with `getTargetUser`

--- slashcommands/admins.go
+++ slashcommands/admins.go
@@ -4,6 +4,7 @@ import (
 	"MythiccBotHyper/datatype"
 	"MythiccBotHyper/globals"
 	"MythiccBotHyper/model"
+	"errors"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"log"
@@ -67,11 +68,19 @@ func admins(state *discordgo.Session, interaction *discordgo.InteractionCreate) 
 			contentMessage = adminsList()
 			break
 		case "add":
-			targetUser := options[0].Options[0].UserValue(state)
+			targetUser, err := getTargetUser(state, options)
+			if err != nil {
+				contentMessage = err.Error()
+				break
+			}
 			contentMessage = adminsAdd(targetUser)
 			break
 		case "remove":
-			targetUser := options[0].Options[0].UserValue(state)
+			targetUser, err := getTargetUser(state, options)
+			if err != nil {
+				contentMessage = err.Error()
+				break
+			}
 			contentMessage = adminsRemove(targetUser)
 			break
 		default:
@@ -137,4 +146,16 @@ func adminsAdd(user *discordgo.User) string {
 	}
 
 	return fmt.Sprintf("<@%v> was added...", id)
+}
+
+func getTargetUser(state *discordgo.Session, options []*discordgo.ApplicationCommandInteractionDataOption) (*discordgo.User, error) {
+	if options[0] != nil {
+		if options[0].Options[0] != nil {
+			if options[0].Options[0].UserValue(state) != nil {
+				return options[0].Options[0].UserValue(state), nil
+			}
+		}
+	}
+
+	return nil, errors.New("target user missing from command or state")
 }
