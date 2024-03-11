4226ab29a21fcda6c4f478795fb79edd1c8d0452
HyperAlch <Daemonleak@protonmail.com>
2024-03-11 16:42:15 -0400

    Move db actions to separate files

--- db/db.go
+++ db/db.go
@@ -25,39 +25,10 @@ func init() {
 }
 
 func createTables() {
-	createAdminsTables := `
-	CREATE TABLE IF NOT EXISTS admins (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		snowflake TEXT NOT NULL UNIQUE
-	)
-	`
-
-	_, err := DB.Exec(createAdminsTables)
-	if err != nil {
-		log.Fatal("Could not create admins table!")
-	}
-
-	checkMasterAdmin := "SELECT snowflake FROM admins WHERE snowflake = ?"
-	row := DB.QueryRow(checkMasterAdmin, globals.MasterAdmin)
-
-	var snowflake string
-	_ = row.Scan(&snowflake)
-
-	if snowflake == "" {
-		insertMasterAdminQuery := "INSERT INTO admins(snowflake) VALUES (?)"
-		_, err = DB.Exec(insertMasterAdminQuery, globals.MasterAdmin)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
+	createAdminsTable()
+	createGamesTable()
 }
 
 func dropTables() {
-	dropAdminsTables := "DROP TABLE IF EXISTS admins"
-
-	_, err := DB.Exec(dropAdminsTables)
-	if err != nil {
-		log.Println("Could not drop admins table!")
-	}
+	dropAdminsTable()
 }
