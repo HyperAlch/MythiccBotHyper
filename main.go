dee98d00c2d9157e3801de21dc711fe399e7650b
HyperAlch <Daemonleak@protonmail.com>
2024-03-13 21:01:52 -0400

    Unneeded messageCreate handler removed

--- main.go
+++ main.go
@@ -27,7 +27,6 @@ func main() {
 		}
 	}(db.DB)
 
-	g.Bot.AddHandler(messageCreate)
 	g.Bot.AddHandler(interactionCreate)
 
 	g.Bot.Identify.Intents = discordgo.IntentsGuilds |
@@ -65,26 +64,6 @@ func main() {
 		}
 	}(g.Bot)
 
-}
-
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the authenticated bot has access to.
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example, but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
 }
 
 const messageComponent = 3 /* Button press, dropdown select, etc 	*/
