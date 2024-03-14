096229445d77245ae506f2dc4bbae9b585704d50
HyperAlch <Daemonleak@protonmail.com>
2024-03-13 20:59:25 -0400

    `pickGamesAddExecute` and `pickGamesRemoveExecute` ready to impl

--- messageComponents/messageComponents.go
+++ messageComponents/messageComponents.go
@@ -6,7 +6,9 @@ import (
 
 var (
 	MessageComponentHandlers = datatype.InteractionMap{
-		"pick-games-add":    pickGamesAdd,
-		"pick-games-remove": pickGamesRemove,
+		"pick-games-add":            pickGamesAdd,
+		"pick-games-remove":         pickGamesRemove,
+		"pick-games-add-execute":    pickGamesAddExecute,
+		"pick-games-remove-execute": pickGamesRemoveExecute,
 	}
 )
---
+++ messageComponents/pickGamesExecute.go
@@ -0,0 +1,11 @@
+package messageComponents
+
+import "github.com/bwmarrin/discordgo"
+
+func pickGamesAddExecute(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+
+}
+
+func pickGamesRemoveExecute(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+
+}
