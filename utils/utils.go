1afadd2c2c0686eebc0a96b04473327fd6f935b0
HyperAlch <Daemonleak@protonmail.com>
2024-04-07 00:25:40 -0400

    DateDiff Util created

--- utils/utils.go
+++ utils/utils.go
@@ -2,9 +2,13 @@ package utils
 
 import (
 	"fmt"
-	"github.com/bwmarrin/discordgo"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func Filter[T any](items []T, fn func(item T) bool) []T {
@@ -29,4 +33,45 @@ func GetAvatarUrl(user *discordgo.User) (string, error) {
 	}
 
 	return url, nil
+}
+
+func DateDiff(date time.Time) (time.Time, error) {
+	dateStr := date.String()
+	dateStr = dateStr[0:10]
+	joinDateArray := strings.Split(dateStr, "-")
+	var joinDateIntArray []int
+
+	for _, numStr := range joinDateArray {
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			return time.Time{}, err
+		}
+		joinDateIntArray = append(joinDateIntArray, num)
+	}
+	joinDate := time.Date(
+		joinDateIntArray[0],
+		time.Month(joinDateIntArray[1]),
+		joinDateIntArray[2],
+		0, 0, 0, 0, time.UTC,
+	)
+	currentDate := time.Now().UTC()
+	currentDate = time.Date(
+		currentDate.Year(),
+		currentDate.Month(),
+		20,
+		0, 0, 0, 0, time.UTC,
+	)
+
+	days := int(currentDate.Sub(joinDate).Hours() / 24)
+	years := int(days / 365)
+	remaining_days := int(days % 365)
+	months := int(remaining_days / 30)
+	days = int(remaining_days % 30)
+
+	return time.Date(
+		years,
+		time.Month(months),
+		days,
+		0, 0, 0, 0, time.UTC,
+	), nil
 }
