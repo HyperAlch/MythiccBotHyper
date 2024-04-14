e02b817d36231a9ebc83f2f2ed63bc7582f87c6a
HyperAlch <Daemonleak@protonmail.com>
2024-04-14 18:21:13 -0400

    guildMemberRemoved done

--- majorLogs/guildMemberRemove.go
+++ majorLogs/guildMemberRemove.go
@@ -1,18 +1,78 @@
 package majorlogs
 
 import (
+	g "MythiccBotHyper/globals"
+	"MythiccBotHyper/interactives"
+	"MythiccBotHyper/utils"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func GuildMemberRemove(*discordgo.Session, *discordgo.GuildMemberRemove) {
+func GuildMemberRemove(session *discordgo.Session, guildMemberRemoveData *discordgo.GuildMemberRemove) {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Bot Recovered:", r)
 		}
 	}()
-	// TODO
-	log.Println("Guild member left...")
+
+	user := guildMemberRemoveData.User
+	joinDate, err := discordgo.SnowflakeTimestamp(user.ID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	dateDiff, err := utils.DateDiff(joinDate)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	years := dateDiff.Year()
+	months := int(dateDiff.Month())
+	days := int(dateDiff.Day())
+
+	formattedDateDiff := fmt.Sprintf(
+		"**%v** ***years*** | **%v** ***months*** | **%v** ***days***",
+		years, months, days,
+	)
+
+	fields := []*discordgo.MessageEmbedField{
+		{
+			Name:   "Account Age",
+			Value:  formattedDateDiff,
+			Inline: true,
+		},
+	}
+
+	url, _ := utils.GetAvatarUrl(user)
+	userIdText := fmt.Sprintf("User ID: %v", user.ID)
+	timeStamp := time.Now().Format(time.RFC3339)
+
+	_, err = session.ChannelMessageSendEmbed(
+		g.MajorEventsChannel,
+		&discordgo.MessageEmbed{
+			Title:       "Member Left",
+			Color:       0xED4245,
+			Fields:      fields,
+			Description: interactives.FromUserId(user.ID),
+			Image: &discordgo.MessageEmbedImage{
+				URL: "https://i.ibb.co/1qyVmzG/left-discord.png",
+			},
+			Author: &discordgo.MessageEmbedAuthor{
+				Name:    user.Username,
+				IconURL: url,
+			},
+			Footer: &discordgo.MessageEmbedFooter{
+				Text: userIdText,
+			},
+			Timestamp: timeStamp,
+		},
+	)
+	if err != nil {
+		return
+	}
 }
