b176a088fbe9d8981c69a0fa5a0733030928faaa
HyperAlch <Daemonleak@protonmail.com>
2024-04-14 18:33:50 -0400

    guildMemberUnbanned done

--- majorLogs/guildMemberUnbanned.go
+++ majorLogs/guildMemberUnbanned.go
@@ -1,18 +1,79 @@
 package majorlogs
 
 import (
+	g "MythiccBotHyper/globals"
+	"MythiccBotHyper/interactives"
+	"MythiccBotHyper/utils"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func GuildMemberUnbanned(*discordgo.Session, *discordgo.GuildBanRemove) {
+func GuildMemberUnbanned(session *discordgo.Session, guildMemberBanRemovedData *discordgo.GuildBanRemove) {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Bot Recovered:", r)
 		}
 	}()
-	// TODO
-	log.Println("Guild member unbanned...")
+
+	user := guildMemberBanRemovedData.User
+	joinDate, err := discordgo.SnowflakeTimestamp(user.ID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	dateDiff, err := utils.DateDiff(joinDate)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	years := dateDiff.Year()
+	months := int(dateDiff.Month())
+	days := int(dateDiff.Day())
+
+	formattedDateDiff := fmt.Sprintf(
+		"**%v** ***years*** | **%v** ***months*** | **%v** ***days***",
+		years, months, days,
+	)
+
+	fields := []*discordgo.MessageEmbedField{
+		{
+			Name:   "Account Age",
+			Value:  formattedDateDiff,
+			Inline: true,
+		},
+	}
+
+	url, _ := utils.GetAvatarUrl(user)
+	userIdText := fmt.Sprintf("User ID: %v", user.ID)
+	timeStamp := time.Now().Format(time.RFC3339)
+
+	_, err = session.ChannelMessageSendEmbed(
+		g.MajorEventsChannel,
+		&discordgo.MessageEmbed{
+			Title:       "Member Unbanned",
+			Color:       0x57F287,
+			Fields:      fields,
+			Description: interactives.FromUserId(user.ID),
+			Image: &discordgo.MessageEmbedImage{
+				URL: "https://i.ibb.co/7nqVFKd/unbanned.png",
+			},
+			Author: &discordgo.MessageEmbedAuthor{
+				Name:    user.Username,
+				IconURL: url,
+			},
+			Footer: &discordgo.MessageEmbedFooter{
+				Text: userIdText,
+			},
+			Timestamp: timeStamp,
+		},
+	)
+	if err != nil {
+		return
+	}
+
 }
