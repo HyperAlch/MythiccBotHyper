3c00f34b0e51861da7db2f4ed7f1cbac4f49981b
HyperAlch <Daemonleak@protonmail.com>
2024-04-20 19:32:03 -0400

    Guild apply modal popup half done

--- applicationCommands/applicationCommands.go
+++ applicationCommands/applicationCommands.go
@@ -1,11 +1,57 @@
 package applicationcommands
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"MythiccBotHyper/globals"
+	"slices"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 var (
-	guildApplyDetails = discordgo.ApplicationCommand{
-		Name:        "Guild Apply",
-		Description: "Apply for guild membership",
-		Type:        discordgo.MessageApplicationCommand,
+	GuildApplyDetails = discordgo.ApplicationCommand{
+		Name: "Guild Apply",
+		Type: discordgo.UserApplicationCommand,
 	}
 )
+
+func GuildApply(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
+	hasApplyRole := slices.Contains(interaction.Member.Roles, globals.NeedsToApplyRole)
+
+	responseType := discordgo.InteractionResponseModal
+	responseData := &discordgo.InteractionResponseData{}
+
+	if !hasApplyRole {
+		responseType = discordgo.InteractionResponseChannelMessageWithSource
+		responseData = &discordgo.InteractionResponseData{
+			Content: "Application already sent OR you never selected a supported game while joining",
+			Flags:   discordgo.MessageFlagsEphemeral,
+		}
+	} else {
+		responseData = &discordgo.InteractionResponseData{
+			CustomID: "guild_apply_modal",
+			Title:    "Apply To Guild",
+			Components: []discordgo.MessageComponent{
+				discordgo.ActionsRow{
+					Components: []discordgo.MessageComponent{
+						discordgo.TextInput{
+							CustomID:    "in_game_name",
+							Label:       "Your EXACT in-game name",
+							Style:       discordgo.TextInputShort,
+							Placeholder: "Sperg Warrior",
+							Required:    true,
+							MaxLength:   32,
+							MinLength:   1,
+						},
+					},
+				},
+			},
+		}
+	}
+
+	// TODO: Receive the modal response inside a different function
+
+	session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+		Type: responseType,
+		Data: responseData,
+	})
+}
